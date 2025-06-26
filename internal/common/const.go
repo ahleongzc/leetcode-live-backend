24f2f1a65180fb49d8d50ee9f51b0674e7454974
Leong Zhe Cheng <zhecheng555@gmail.com>
2025-06-27 01:53:22 +0800

    feat: implement session repo

--- internal/common/const.go
+++ internal/common/const.go
@@ -3,7 +3,10 @@ package common
 import "time"
 
 const (
-	ENVIRONMENT_KEY string = "ENV"
+	// Environment
+	ENVIRONMENT_KEY  string = "ENV"
+	DEV_ENVIRONMENT  string = "development"
+	PROD_ENVIRONMENT string = "production"
 
 	// Server
 	PORT_KEY              string = "PORT"
@@ -12,10 +15,11 @@ const (
 	WRITE_TIMEOUT_SEC_KEY string = "WRITE_TIMEOUT"
 
 	// Database
-	DB_DSN_KEY               string = "POSTGRES_DSN"
-	DB_MAX_OPEN_CONN_KEY     string = "POSTGRES_MAX_OPEN_CONN"
-	DB_MAX_IDLE_CONN_KEY     string = "POSTGRES_MAX_IDLE_CONN"
-	DB_MAX_IDLE_TIME_SEC_KEY string = "POSTGRES_MAX_IDLE_TIME_SEC"
+	DB_DSN_KEY               string        = "POSTGRES_DSN"
+	DB_MAX_OPEN_CONN_KEY     string        = "POSTGRES_MAX_OPEN_CONN"
+	DB_MAX_IDLE_CONN_KEY     string        = "POSTGRES_MAX_IDLE_CONN"
+	DB_MAX_IDLE_TIME_SEC_KEY string        = "POSTGRES_MAX_IDLE_TIME_SEC"
+	DB_QUERY_TIMEOUT         time.Duration = 1 * time.Second
 
 	// Cloudflare R2
 	R2_ACCESS_KEY   string = "R2_ACCESS_KEY"
@@ -24,8 +28,14 @@ const (
 	R2_BUCKET_KEY   string = "R2_BUCKET"
 	R2_REGION_KEY   string = "R2_REGION"
 
-	DEV_ENVIRONMENT  string = "development"
-	PROD_ENVIRONMENT string = "production"
+	// Tables
+	SESSION_TABLE_NAME string = "sessions"
+	USER_TABLE_NAME    string = "users"
+)
 
-	DB_QUERY_TIMEOUT time.Duration = 1 * time.Second
+var (
+	TRUSTED_ORIGINS = map[string]struct{}{
+		"localhost": {},
+		"0.0.0.0":   {},
+	}
 )
--- internal/repo/session.go
+++ internal/repo/session.go
@@ -1,15 +1,19 @@
 package repo
 
 import (
+	"context"
 	"database/sql"
+	"errors"
+	"fmt"
 
+	"github.com/ahleongzc/leetcode-live-backend/internal/common"
 	"github.com/ahleongzc/leetcode-live-backend/internal/entity"
 )
 
 type SessionRepo interface {
-	GetSessionByID(ID string) *entity.Session
-	CreateSession(session *entity.Session) error
-	DeleteSessionByID(ID string) *entity.Session
+	Create(ctx context.Context, session *entity.Session) error
+	GetByID(ctx context.Context, ID string) (*entity.Session, error)
+	DeleteByID(ctx context.Context, ID string) error
 }
 
 func NewSessionRepoImpl(
@@ -25,16 +29,71 @@ type SessionRepoImpl struct {
 }
 
 // DeleteSessionByID implements SessionRepo.
-func (s *SessionRepoImpl) DeleteSessionByID(ID string) *entity.Session {
-	panic("unimplemented")
+func (s *SessionRepoImpl) DeleteByID(ctx context.Context, ID string) error {
+	ctx, cancel := context.WithTimeout(ctx, common.DB_QUERY_TIMEOUT)
+	defer cancel()
+
+	args := []any{ID}
+
+	query := fmt.Sprintf(`
+		DELETE FROM %s
+		WHERE id = $1
+	`, common.SESSION_TABLE_NAME)
+
+	_, err := s.db.ExecContext(ctx, query, args...)
+	if err != nil {
+		return fmt.Errorf("unable to delete session with id %s, %s: %w", ID, err.Error(), common.ErrInternalServerError)
+	}
+
+	return nil
 }
 
 // CreateSession implements SessionRepo.
-func (s *SessionRepoImpl) CreateSession(session *entity.Session) error {
-	panic("unimplemented")
+func (s *SessionRepoImpl) Create(ctx context.Context, session *entity.Session) error {
+	ctx, cancel := context.WithTimeout(ctx, common.DB_QUERY_TIMEOUT)
+	defer cancel()
+
+	args := []any{session.ID, session.ExpireTimestampMS}
+
+	query := fmt.Sprintf(`
+		INSERT INTO %s
+		(id, expire_timestamp_ms)
+		VALUES
+		($1, $2)
+	`, common.SESSION_TABLE_NAME)
+
+	_, err := s.db.ExecContext(ctx, query, args...)
+	if err != nil {
+		return fmt.Errorf("unable to create new session, %s: %w", err.Error(), common.ErrInternalServerError)
+	}
+
+	return nil
 }
 
 // GetSessionByID implements SessionRepo.
-func (s *SessionRepoImpl) GetSessionByID(ID string) *entity.Session {
-	panic("unimplemented")
+func (s *SessionRepoImpl) GetByID(ctx context.Context, ID string) (*entity.Session, error) {
+	ctx, cancel := context.WithTimeout(ctx, common.DB_QUERY_TIMEOUT)
+	defer cancel()
+
+	args := []any{ID}
+
+	query := fmt.Sprintf(`
+		SELECT 
+			id, expire_timestamp_ms 
+		FROM 
+			%s
+		WHERE 
+			id = $1
+	`, common.SESSION_TABLE_NAME)
+
+	session := &entity.Session{}
+	err := s.db.QueryRowContext(ctx, query, args...).Scan(&session.ID, &session.ExpireTimestampMS)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, fmt.Errorf("session: %w", common.ErrNotFound)
+		}
+		return nil, fmt.Errorf("unable to get session with id %s, %s: %w", ID, err.Error(), common.ErrInternalServerError)
+	}
+
+	return session, nil
 }
